8b8c1047a4c530ccb7e99baba15d0218a1f47659
Michael McGrain <153415900+mrmcgrain@users.noreply.github.com>
2025-01-08 13:53:24 -0700

    stuff

---
+++ week01/callMeBacks/callbacks.js
@@ -0,0 +1,31 @@
+
+
+    const sum = (num1, num2) => {
+        console.log("1", num1, "2", num2)
+        return num1 + num2
+    }
+
+    console.log(sum(2, 2))
+
+    const sum2 = (num1, num2) => {
+        console.log("1", num1, "2", num2)
+        return num1 + num2
+    }
+
+    const area = (input, callback) => {
+
+        console.log("input", input)
+        console.log("callback", callback)
+        // return "area return" + " " +  input * callback
+        return `area return:`  + input * callback
+    }
+
+    console.log("twz", area(20 , sum( 20, sum2(20, 10000))))
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// let len = arr.length
+
+//     for (let i = 0 ; i < len ; i++){
+//   //do stuff
+//     }
