36749a2c5c4e132c5f5aa84b980ff85e21c938c1
Michael McGrain <153415900+mrmcgrain@users.noreply.github.com>
2025-01-09 10:17:14 -0700

    server template

--- template/server.js
+++ template/server.js
@@ -5,15 +5,26 @@ const express = require('express');
 // Initialize the app
 const app = express();
 
+app.use(express.json()); // Middleware to parse JSON body
+
 // Define a port
 const PORT = 3000;
 
 // Define a simple route
 app.get('/', (req, res) => {  //req, res cycle
-    res.send('Hello, world!');  //response
+    console.log("GET route hit / - ")
+    res.send('From Get req on Server!');  //response
 });
+
+
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
+
+
+
