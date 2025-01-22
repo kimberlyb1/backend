49af319b34faeba2917314a4a4a576206f1c5bcf
Michael McGrain <153415900+mrmcgrain@users.noreply.github.com>
2025-01-22 13:49:17 -0700

    DB

--- lectures/Todo/client/Todo/src/App.jsx
+++ lectures/Todo/client/Todo/src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+
+  const [data, setData] = useState()
+
+  useEffect(() => {
+
+    axios({
+      method: "get",
+      url: "http://localhost:3000/gettodos"
+    })
+      .then(res => {
+        console.log("res", res)
+        setData(res.data)
+      })
+      .catch(err => console.log("err", err))
+
+
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {console.log("data", data)}
+
+   
+        {data && data.map((item) => {
+          return (
+            <div style={{ border: '2px solid red' }}>
+
+            <p> {item.todo}</p>
+            <button>delete</button>
+            <button>edit</button>
+         
+            </div>
+          )
+        })}
+
     </>
   )
 }
