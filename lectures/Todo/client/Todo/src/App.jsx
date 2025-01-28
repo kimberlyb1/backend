0006c33e46241e44709d115ac7b64d28c7908c13
Michael McGrain <153415900+mrmcgrain@users.noreply.github.com>
2025-01-27 17:45:37 -0700

    app.jsx

--- lectures/Todo/client/Todo/src/App.jsx
+++ lectures/Todo/client/Todo/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import axios from 'axios'
 
@@ -9,6 +8,7 @@ function App() {
 
   const [data, setData] = useState()
 
+  
   const [newToDo, setNewToDo] = useState(
     {
       todo: "",
