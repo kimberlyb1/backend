0d14aded0375ddc066f95a0a5e7590ce338944e1
Kimberly Bridges <kbridges.prsvr@gmail.com>
2025-02-05 13:26:02 -0600

    Co-authored-by: Michael McGrain <mrmcgrain@users.noreply.github.com>

--- midterm/server/server.js
+++ midterm/server/server.js
@@ -12,7 +12,7 @@ app.use(express.json()); // Parses incoming JSON requests
 console.log('JSON body parser enabled');
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log('Database connection error:', err));
 
